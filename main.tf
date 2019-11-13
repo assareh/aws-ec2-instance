1dbc9e92dacf0eef9bf064142330b83b9b781413
assareh <assareh@hashicorp.com>
2019-11-12 17:31:24 -0800

    tf fmt

--- main.tf
+++ main.tf
@@ -7,8 +7,8 @@ provider "aws" {
 }
 
 resource "aws_instance" "ubuntu" {
-  ami               = var.ami_id
-  instance_type     = var.instance_type
+  ami           = var.ami_id
+  instance_type = var.instance_type
 
   tags = {
     Name = var.name
