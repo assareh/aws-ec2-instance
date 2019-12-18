eb4d23138f7abe626deb2108320396bcfd67eb45
assareh <assareh@hashicorp.com>
2019-12-18 12:54:31 -0800

    large

--- variables.tf
+++ variables.tf
@@ -10,7 +10,7 @@ variable "ami_id" {
 
 variable "instance_type" {
   description = "type of EC2 instance to provision."
-  default     = "t2.nano"
+  default     = "m4.large"
 }
 
 variable "tags" {
